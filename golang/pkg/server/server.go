67aac61829b9e3f74717d91e19a66c77072be738
Renato Moitinho <renato.dias@gmail.com>
2024-03-04 01:58:05 +0000

    refactorin

--- golang/pkg/server/server.go
+++ golang/pkg/server/server.go
@@ -2,7 +2,10 @@ package server
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 type Server struct {
@@ -14,6 +17,7 @@ func (s *Server) Home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	_ = json.NewEncoder(w).Encode(map[string]string{
 		"message": "We will rock you!! 🤘",
+		"uuid":    uuid.NewString(),
 	})
 }
 
@@ -21,5 +25,6 @@ func (s *Server) Start() error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", s.Home)
 	srv := &http.Server{Addr: s.Addr, Handler: mux}
+	log.Printf("Server is running on %s", s.Addr)
 	return srv.ListenAndServe()
 }
