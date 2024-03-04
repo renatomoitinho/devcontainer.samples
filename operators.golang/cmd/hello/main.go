34e30e14fe60c2e50e2524badf29e5ef3826634a
Renato Moitinho <renato.dias@gmail.com>
2024-03-04 02:20:02 +0000

    refactoring

--- operators.golang/cmd/hello/main.go
+++ operators.golang/cmd/hello/main.go
@@ -1,31 +1,11 @@
 package main
 
 import (
-	"encoding/json"
+	"hello/pkg/server"
 	"log"
-	"net/http"
 )
 
-type Server struct {
-	Addr string
-}
-
-func (s *Server) Home(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	_ = json.NewEncoder(w).Encode(map[string]string{
-		"message": "We will rock you!! 🤘",
-	})
-}
-
-func (s *Server) Start() error {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", s.Home)
-	srv := &http.Server{Addr: s.Addr, Handler: mux}
-	return srv.ListenAndServe()
-}
-
 func main() {
-	s := &Server{Addr: ":8080"}
+	s := &server.Server{Addr: ":8080"}
 	log.Fatal(s.Start())
 }
